ad194a447d91096c5d039b49bc3fce86e09f59e4
AdventGit <21107733+AdventGit@users.noreply.github.com>
2018-04-09 02:14:41 -0500

    Fixes and release of a tool to log you out of Discord upon closing it

---
+++ Auto-Logout Discord/Auto-Logout Discord.bat
@@ -0,0 +1,10 @@
+@echo off
+title Auto-Logout Discord
+mode con:cols=13 lines=1
+set "Discord=%localappdata%\DiscordPTB\app-0.0.41"
+set "DataBase=%appdata%/discordptb/Local Storage"
+sqlite3.exe "%DataBase%/https_ptb.discordapp.com_0.localstorage" < commands.txt
+cd "%Discord%"
+start /wait DiscordPTB.exe
+sqlite3.exe "%DataBase%/test.db" < commands.txt
+exit

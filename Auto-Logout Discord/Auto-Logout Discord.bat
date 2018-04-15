d2087aa11ceb5b1e1713585d2bfc04912bb638f7
AdventGit <21107733+AdventGit@users.noreply.github.com>
2018-04-14 19:38:03 -0500

    Updating for 0.0.42

--- Auto-Logout Discord/Auto-Logout Discord.bat
+++ Auto-Logout Discord/Auto-Logout Discord.bat
@@ -1,7 +1,7 @@
 @echo off
 title Auto-Logout Discord
 mode con:cols=13 lines=1
-set "Discord=%localappdata%\DiscordPTB\app-0.0.41"
+set "Discord=%localappdata%\DiscordPTB\app-0.0.42"
 set "DataBase=%appdata%/discordptb/Local Storage"
 sqlite3.exe "%DataBase%/https_ptb.discordapp.com_0.localstorage" < commands.txt
 cd "%Discord%"

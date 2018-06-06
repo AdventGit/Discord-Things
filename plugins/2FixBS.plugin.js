684cdb5fe765c7b106c4d921f0a3d81145b629e9
AdventGit <21107733+AdventGit@users.noreply.github.com>
2018-06-06 09:39:12 -0500

    Stuff and things

--- plugins/2FixBS.plugin.js
+++ plugins/2FixBS.plugin.js
@@ -12,12 +12,17 @@ class FixBS {
         var libraryScript2 = null;
         if (typeof BDFDB === "object") BDFDB = "";
         libraryScript = document.querySelector('head script[src="https://mwittrien.github.io/BetterDiscordAddons/Plugins/BDFDB.js"]');
+        libraryScript = document.querySelector('head script[src="https://mwittrien.github.io/BetterDiscordAddons/Plugins/BDfunctionsDevilBro.js"]');
         if (libraryScript) libraryScript.remove();
+        if (libraryScript2) libraryScript2.remove();
         libraryScript = document.createElement("script");
+        libraryScript2 = document.createElement("script");
         libraryScript.setAttribute("type", "text/javascript");
+        libraryScript2.setAttribute("type", "text/javascript");
         libraryScript.setAttribute("src", "https://rawgit.com/AdventGit/BetterDiscordAddons/patch-1/Plugins/BDFDB.js");
+        libraryScript2.setAttribute("src", "https://rawgit.com/AdventGit/BetterDiscordAddons/patch-1/Plugins/BDfunctionsDevilBro.js");
         document.head.appendChild(libraryScript);
-		}
+        document.head.appendChild(libraryScript2);
 		this.startTimeout = setTimeout(() => {this.initialize();}, 30000);
 		if (typeof BDFDB === "object") this.initialize();
 		else libraryScript.addEventListener("load", () => {this.initialize();});

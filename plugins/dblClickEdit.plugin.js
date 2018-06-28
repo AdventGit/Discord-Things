398658e21684453953c7d11119c3f64c952a6cb6
AdventGit <21107733+AdventGit@users.noreply.github.com>
2018-06-28 15:34:58 -0500

    Good shit

--- plugins/dblClickEdit.plugin.js
+++ plugins/dblClickEdit.plugin.js
@@ -1,11 +1,8 @@
 //META{"name":"dblClickEdit"}*//
 
 var dblClickEdit = function () {};
-
-dblClickEdit.prototype.onMessage = function () {
-};
-dblClickEdit.prototype.onSwitch = function () {
-};
+dblClickEdit.prototype.onMessage = function () {};
+dblClickEdit.prototype.onSwitch = function () {};
 dblClickEdit.prototype.start = function () {
     $(document).on("dblclick.dce", function(e) {
         var target = $(e.target);
@@ -13,18 +10,16 @@ dblClickEdit.prototype.start = function () {
             var msg = target.parents(".message").first();
             var opt = msg.find(".btn-option");
             opt.click();
-
-            $.each($(".option-popout .btn-item"), (index, value) => {
-                var option = $(value);
-                if(option.text() === "Edit") {
-                    option.click();
+            $.each($('.container-1rPqdX [class*="button"]'), (index, value) => {
+                var menuitem = $(value);
+                if(menuitem.text() === "Edit") {
+                    menuitem.click();
+                    return;
                 }
             });
-
         }
     });
 };
-
 dblClickEdit.prototype.load = function () {};
 dblClickEdit.prototype.unload = function () {
     $(document).off("dblclick.dce");
@@ -32,18 +27,14 @@ dblClickEdit.prototype.unload = function () {
 dblClickEdit.prototype.stop = function () {
     $(document).off("dblclick.dce");
 };
-dblClickEdit.prototype.getSettingsPanel = function () {
-    return "";
-};
-
 dblClickEdit.prototype.getName = function () {
-    return "Double click edit";
+    return "Double Click Edit";
 };
 dblClickEdit.prototype.getDescription = function () {
-    return "Double click messages to edit them";
+    return "Double Click your own messages to edit them";
 };
 dblClickEdit.prototype.getVersion = function () {
-    return "0.1.2";
+    return "0.1.3";
 };
 dblClickEdit.prototype.getAuthor = function () {
     return "Jiiks (AdventDiscord Edit)";

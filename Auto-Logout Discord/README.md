46f0b83104999487a83c0eb7245e6e0aadf0d70f
AdventGit <21107733+AdventGit@users.noreply.github.com>
2019-01-18 03:37:13 -0600

    Yeets

---
+++ Auto-Logout Discord/README.md
@@ -0,0 +1,5 @@
+# Auto-Logout Discord
+
+This will log you out of Discord by deleting your saved Token. You can either use the provided MSI made by CXFreeze or you can build yourself using Py36.
+
+leveldb.pyd provided by https://github.com/happynear/py-leveldb-windows
--- Auto-Logout Discord/setup.py
+++ Auto-Logout Discord/setup.py
@@ -3,6 +3,8 @@ from os import environ
 from os import getcwd as gcd
 from cx_Freeze import setup, Executable
 
+target_folder = str('')#The folder you stuck "Auto-Logout Discord.py" in
+
 build_exe_options = {'packages':['os'],'excludes':['tkinter']}
 environ['TCL_LIBRARY'] = r'C:\Program Files\Python\tcl\tcl8.6'
 environ['TK_LIBRARY'] = r'C:\Program Files\Python\tcl\tk8.6'
@@ -11,4 +13,4 @@ base = None
 if platform == 'win32':
     base = 'Win32GUI'
 
-setup(name='Auto-Logout Discord',version='1.0',description='Build 0.0.46 Compliant',options={'build_exe':build_exe_options},executables=[Executable('C:\\Users\\Advent\\Desktop\\Plyvel Shit\\scripts\\Auto-Logout Discord.py',base=base)])
+setup(name='Auto-Logout Discord',version='1.0',description='Build 0.0.46 Compliant',options={'build_exe':build_exe_options},executables=[Executable(str(str(target_folder) + str('Auto-Logout Discord.py')),base=base)])

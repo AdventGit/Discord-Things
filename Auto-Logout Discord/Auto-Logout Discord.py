5984581efa1a3373a772d55b2d2044b47d1f0d7d
AdventGit <21107733+AdventGit@users.noreply.github.com>
2019-01-18 05:24:31 -0600

    Yeets

--- Auto-Logout Discord/Auto-Logout Discord.py
+++ Auto-Logout Discord/Auto-Logout Discord.py
@@ -7,7 +7,7 @@ cd(expvar(r'%appdata%\discordptb\Local Storage'))
 wldb = ldb('leveldb',create_if_missing=False)
 wldb.Delete(b'_https://ptb.discordapp.com\x00\x01token')
 del wldb
-spr(['cmd','/c','start','/wait','',r'C:\Users\Advent\AppData\Local\DiscordPTB\app-0.0.46\DiscordPTB.exe'],shell=True,universal_newlines=True)
+spr(['cmd','/c','start','/wait','',expvar(r'%localappdata%\DiscordPTB\app-0.0.46\DiscordPTB.exe')],shell=True,universal_newlines=True)
 wldb = ldb('leveldb',create_if_missing=False)
 wldb.Delete(b'_https://ptb.discordapp.com\x00\x01token')
 del wldb

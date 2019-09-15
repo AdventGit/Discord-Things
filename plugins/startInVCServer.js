6660d4723eaf11cc27693c1b29815afaa2d5acf8
Advent <21107733+AdventGit@users.noreply.github.com>
2019-09-14 21:12:28 -0600

    Rename rebased plugin

---
+++ plugins/startInVCServer.js
@@ -0,0 +1,24 @@
+//META{"name":"startInVCServer"}*//
+
+const startInVCServer = function () {};
+
+startInVCServer.prototype.getName = function () {
+    return 'Auto-Select Server';
+}
+startInVCServer.prototype.getDescription = function () {
+    return 'Selects the server you'+"'"+'re speaking in on startup.';
+}
+startInVCServer.prototype.getVersion = function () {
+    return '2.0';
+}
+startInVCServer.prototype.getAuthor = function () {
+    return 'AdventDiscord (TonyLemur Inspired)';
+}
+
+startInVCServer.prototype.start = function() {
+    try {
+        $('div.labelWrapper-Pniq53 > a')[0].click();
+    } catch {}
+}
+
+startInVCServer.prototype.stop = function() {}
